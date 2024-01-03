963c94ba91aaa571c1be4094b2329e51a4b40ffd
PJacketG <anantamiyoruwijaya2@gmail.com>
2024-01-03 11:08:20 +0700

    Init

---
+++ Assets/!GameAssets/Scripts/New Class.cs
@@ -0,0 +1,27 @@
+//-----------------------------------------------------------------------
+// Author   : ""
+//----------------------------------------------------------------------
+
+using UnityEngine;
+
+
+namespace ProjectB
+{
+    /// <summary>
+    /// Class summary
+    /// </summary>
+    public class NewClass : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
