9802ac50a9f8ea104a48a59fc8acd02249437e84
PJacketG <anantamiyoruwijaya2@gmail.com>
2024-02-05 10:46:46 +0700

    refactor: change asset placement in the folder

---
+++ Assets/!GameAssets/_Srcs/Scripts/Player/PlayerResouce.cs
@@ -0,0 +1,32 @@
+//----------------------------------------------------------------------
+// Author   : "Ananta Miyoru Wijaya"
+// Created  : "2024/02/02"
+//----------------------------------------------------------------------
+
+using UnityEngine;
+
+
+namespace UnderworldCafe
+{
+    /// <summary>
+    /// Class is responsible for managing player resources
+    /// </summary>
+    public class PlayerResouce : MonoBehaviour
+    {
+        private int _money;
+        public int Money => _money;
+
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            
+        }
+    }
+}
