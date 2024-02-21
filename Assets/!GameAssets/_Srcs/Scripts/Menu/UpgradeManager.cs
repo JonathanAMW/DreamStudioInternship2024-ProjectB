1c229460e4f30f86b84a7f6e8aaeeeb146529b22
VanFL <vanfl.0w0@gmail.com>
2024-02-21 17:05:19 +0700

    Feat: Upgrade Menu Pages
    Implemented buttons to go through upgrade menu pages

--- Assets/!GameAssets/_Srcs/Scripts/Menu/UpgradeManager.cs
+++ Assets/!GameAssets/_Srcs/Scripts/Menu/UpgradeManager.cs
@@ -3,6 +3,10 @@
 // Created  : "2024/02/19"
 //----------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,15 +17,106 @@ namespace UnderworldCafe
     /// </summary>
     public class UpgradeManager : MonoBehaviour
     {
-        public string wareName1;
+        public TextMeshProUGUI wareName1;
         public GameObject[] grades1=new GameObject[3];
         public Image wareImage1;
-        public string wareName2;
+        public TextMeshProUGUI wareName2;
         public GameObject[] grades2 = new GameObject[3];
         public Image wareImage2;
-        public string wareName3;
+        public TextMeshProUGUI wareName3;
         public GameObject[] grades3 = new GameObject[3];
         public Image wareImage3;
         public KitchenWare[] kitchenWares; //lists of kitchen wares
+
+        [SerializeField] GameObject[] upgradeItemObjects= new GameObject[3];
+
+        KitchenWare[] currentKitchenWares= new KitchenWare[3];
+
+        public List<int[]> kitchenWaresIndexCollection= new List<int[]>();
+        public int currentIndexNumber=0;
+        int[] currentIndexCollection = new int[3];
+        private void Awake()
+        {
+            //assign kitchewareindexcollection based on kitchenwares
+            int j = 0;
+            for (int i = 0; i < kitchenWares.Length; i++)
+            {
+                List<int> indexCollection=new List<int>();
+                if(j<kitchenWares.Length)
+                {
+                    indexCollection.Add(j);
+                    if (j + 1 < kitchenWares.Length)
+                    {
+                        indexCollection.Add(j+1);
+
+                        if (j + 2 < kitchenWares.Length)
+                        {
+                            indexCollection.Add(j+2);
+                        }
+                    }
+
+                    kitchenWaresIndexCollection.Add(indexCollection.ToArray());
+                    j = j + 3;
+                }
+
+
+            }
+
+            currentIndexNumber = 0;//currentKitchenWares is the first 3 wares
+
+
+            UpdateWareAppearance();
+        }
+
+        public void UpdateWareAppearance()
+        {
+            currentIndexCollection = kitchenWaresIndexCollection[currentIndexNumber]; 
+            for (int i = 0; i < currentKitchenWares.Length; i++) //assigns currentKitchenWares
+            {
+
+                if (i < kitchenWaresIndexCollection[currentIndexNumber].Length)
+                {
+                    currentKitchenWares[i] = kitchenWares[currentIndexCollection[i]];
+                }
+                else
+                {
+                    currentKitchenWares[i] = null;
+                }
+               
+            }
+
+            if (currentKitchenWares[0] !=null)
+            {
+                upgradeItemObjects[0].SetActive(true);
+                wareName1.text = currentKitchenWares[0].wareName;
+                wareImage1.sprite = currentKitchenWares[0].wareImage;
+            }
+            else
+            {
+                upgradeItemObjects[0].SetActive(false);
+            }
+
+            if (currentKitchenWares[1] != null)
+            {
+                upgradeItemObjects[1].SetActive(true);
+                wareName2.text = currentKitchenWares[1].wareName;
+                wareImage2.sprite = currentKitchenWares[1].wareImage;
+            }
+            else
+            {
+                upgradeItemObjects[1].SetActive(false);
+            }
+
+            if (currentKitchenWares[2] != null)
+            {
+                upgradeItemObjects[2].SetActive(true);
+                wareName3.text = currentKitchenWares[2].wareName;
+                wareImage3.sprite = currentKitchenWares[2].wareImage;
+            }
+            else
+            {
+                upgradeItemObjects[2].SetActive(false);
+            }
+        }
     }
 }
--- Assets/!GameAssets/_Srcs/Scripts/Menu/UpgradeMenuManager.cs
+++ Assets/!GameAssets/_Srcs/Scripts/Menu/UpgradeMenuManager.cs
@@ -13,6 +13,21 @@ namespace UnderworldCafe
     /// </summary>
     public class UpgradeMenuManager : MonoBehaviour
     {
+        UpgradeManager _upgradeManager;
+
+        [SerializeField] GameObject nextPageButton;
+        [SerializeField] GameObject prevPageButton;
+
+        private void Awake()
+        {
+            _upgradeManager = FindObjectOfType<UpgradeManager>();
+            
+        }
+
+        private void Start()
+        {
+            ActivatePageButtons();
+        }
         public void ToStageSelect()
         {
             SceneManager.LoadScene("StageSelect");
@@ -21,6 +36,45 @@ namespace UnderworldCafe
         public void OpenOptions()
         {
             Debug.Log("Open options");
+        }
+
+        public void NextUpgradePage()
+        {
+            _upgradeManager.currentIndexNumber++;
+            _upgradeManager.UpdateWareAppearance();
+
+            ActivatePageButtons();
+        }
+
+        public void PrevUpgradePage()
+        {
+            _upgradeManager.currentIndexNumber--;
+            _upgradeManager.UpdateWareAppearance();
+
+            ActivatePageButtons();
+          
+        }
+
+        void ActivatePageButtons()
+        {
+           
+            if (_upgradeManager.currentIndexNumber >= _upgradeManager.kitchenWaresIndexCollection.Count - 1) //on last page
+            {
+                nextPageButton.SetActive(false);
+            }
+            else
+            {
+                nextPageButton.SetActive(true);
+            }
+
+            if (_upgradeManager.currentIndexNumber <= 0) //on first page
+            {
+                prevPageButton.SetActive(false);
+            }
+            else
+            {
+                prevPageButton.SetActive(true);
+            }
         }
     }
 }
