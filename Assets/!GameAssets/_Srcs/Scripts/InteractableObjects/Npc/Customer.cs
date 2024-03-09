c49b8010097d5b68056ea8a855eb4ccae9de63e8
JonathanAMW <94060313+JonathanAMW@users.noreply.github.com>
2024-03-10 01:07:13 +0700

    feat: add preparation for wave and order system and re-added deleted setting
    - add customer and wave manager
    - redirect utensil scripts to Utensil folder
    - re-add namespace setting in Project Setting: Player
    - re-add URP prefab to render setting in Project Setting: Graphic

---
+++ Assets/!GameAssets/_Srcs/Scripts/InteractableObjects/Npc/Customer.cs
@@ -0,0 +1,27 @@
+//----------------------------------------------------------------------
+// Author   : "Ananta Miyoru Wijaya"
+// Created  : "2024/10/03"
+//----------------------------------------------------------------------
+
+using UnityEngine;
+
+namespace UnderworldCafe.CustomerSystem
+{
+    /// <summary>
+    /// Class for defining customer
+    /// </summary>
+    public class Customer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            
+        }
+    }
+}
---
+++ Assets/!GameAssets/_Srcs/Scripts/StandaloneManager/WaveManager.cs
@@ -0,0 +1,25 @@
+//----------------------------------------------------------------------
+// Author   : "InsertYourNameHere"
+// Created  : "YYYY/MM/DD"
+//----------------------------------------------------------------------
+
+using UnityEngine;
+
+
+    /// <summary>
+    /// Class summary
+    /// </summary>
+    public class WaveManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            
+        }
+    }
